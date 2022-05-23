b4f1237519f79ea70c411931ad2cb30405deaa11
pietrik123 <piotrmierzwa042@gmail.com>
2022-05-23 17:56:45 -0400

    * add ut Catch2 submodule * add basic unittest for command tri * add README.MD file

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
+echo "Clearing object files"
 rm *.o
-rm shapedrawer examples
+rm shapedrawer examples unittest
+
+echo "Compiling sources"
 g++ -c -o test.o test.cc
 g++ -c -o command.o command.cc
 g++ -c -o shapes.o shapes.cc
 g++ -c -o main.o main.cc
 g++ -c -o examples.o examples.cc
+
+echo "Linking shapedrawer"
 g++ -o shapedrawer shapes.o command.o main.o
+
+echo "Linking examples"
 g++ -o examples shapes.o test.o examples.o
+
+echo "Building tests"
+g++ -c -o unittest.o ut/unittest.cc -I./ -ICatch2/extras/
+g++ -c -o catch.o Catch2/extras/catch_amalgamated.cpp -ICatch2/extras/
+
+echo "Linking tests"
+g++ -o unittest catch.o unittest.o shapes.o command.o
